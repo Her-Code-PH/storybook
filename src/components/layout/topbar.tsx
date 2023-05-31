d906ebe955345280f1f35a60d9b8087be5f574c8
Bea Aira Lee Corpuz <airaleecorpuz@gmail.com>
2023-05-31 19:56:17 +0800

    chore: topbar

--- src/components/layout/topbar.tsx
+++ src/components/layout/topbar.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Icon from "../../assets/images/reads_logotype.png"
+import Icon from "../../assets/images/reads_logotype.png";
 
 const Topbar = () => {
   return (
     <div className="flex h-[100px] w-full">
-      <div className="p-4 w-full flex justify-between">
+      <div className="px-10 py-5 w-full flex justify-between">
         {/* logo */}
         <div className="logo-container">
           <Image
@@ -13,26 +13,31 @@ const Topbar = () => {
             height={75}
             alt="reads_logotype"
             className='object-cover w-auto h-full'
+            placeholder='empty' // {empty} | {blur}
+            priority={false}
           />
         </div>
         <div className="menu-container flex">
           {/* left menu */}
-          <ul className='flex flex-row items-center'>
-            <li className='uppercase pl-5'>
-              <Link href="/">Home</Link>
+          <ul className='flex flex-row items-center justify-center justify-items-center'>
+            <li className='flex flex-col justify-center h-full uppercase px-5 first:pl-0'>
+              <Link href="/">HOME</Link>
             </li>
-            <li className='uppercase pl-5'>
-              <Link href="/genres">Genre</Link>
+            <li className='flex flex-col justify-center h-full uppercase px-5'>
+              <Link href="/genres">GENRES</Link>
             </li>
-            <li className='uppercase pl-5'>
-              <Link href="/popular">Popular</Link>
+            <li className='flex flex-col justify-center h-full uppercase px-5'>
+              <Link href="/popular">POPULAR</Link>
             </li>
-            <li className='uppercase pl-5'>
-              <Link href="/canvas">Canvas</Link>
+            <li className='flex flex-col justify-center h-full uppercase px-5 last:pr-0'>
+              <Link href="/canvas">CANVAS</Link>
             </li>
           </ul>
           {/* right menu */}
-          <div className=''></div>
+          <div className='py-2 flex flex-row pl-5'>
+            <button className='mr-1 py-2 rounded-full bg-white text-black w-[100px]'>PUBLISH</button>
+            <button className='ml-1 py-2 rounded-full bg-white text-black w-[100px]'>LOGIN</button>
+          </div>
         </div>
       </div>
     </div>
