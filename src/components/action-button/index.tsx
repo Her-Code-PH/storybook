d4b039485f165eda45891da51ca5fcdc724a961f
Bea Aira Lee Corpuz <airaleecorpuz@gmail.com>
2023-05-31 20:14:29 +0800

    chore: action button component - class-variance - tailwind-merge

--- src/components/action-button/index.tsx
+++ src/components/action-button/index.tsx
@@ -1,8 +1,39 @@
-const ActionButton = ({
-  buttonText
+import { cn } from "@/utilities/cs";
+import { cva, VariantProps } from "class-variance-authority"
+import { FC } from "react";
+
+const ButtonVariants = cva(
+  'py-2 rounded-full text-sm w-[100px]',
+  {
+    variants: {
+      type: {
+        outlined: 'border-[1px] border-white text-white',
+        contained: 'bg-white text-black'
+      }
+    },
+    defaultVariants: {
+      type: 'outlined'
+    }
+  }
+)
+
+interface ButtonProps extends VariantProps<typeof ButtonVariants> {
+  buttonText: string,
+  type: 'outlined' | 'contained',
+}
+
+const ActionButton: FC<ButtonProps> = ({
+  buttonText,
+  type,
 }) => {
   return (
-    <button className="fs-14 h-[33px]">{buttonText}</button>
+    <button className={cn(
+      ButtonVariants({
+        type
+      })
+    )}>
+      <span className="text-sm">{buttonText}</span>
+    </button>
   );
 }
  
