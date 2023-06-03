5fd04192d93d2697c5dc10dadb8cfaa6a51d6ed7
Bea Aira Lee Corpuz <airaleecorpuz@gmail.com>
2023-06-03 18:34:59 +0800

    fix: topbar error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <main className='flex min-h-screen'>
           <div className='w-full'>
-            <Topbar />
+            <Topbar user={} onLogin={} onLogout={} />
             {children}
           </div>
         </main>
